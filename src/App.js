d6e42b1e0ebf13af6889bb6f71cefc7f06bbc466
info@deneka.one <69286762+Nic98@users.noreply.github.com>
2024-09-22 15:47:56 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header-text">淘宝 | 极有家 </div>
       </header>
+
+      <div className="contents">
+        <div className="sector"></div>
+      </div>
+      
     </div>
   );
 }
