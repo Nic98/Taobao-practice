3e598e35caec36a3fefafd9854334a8714ca2d56
info@deneka.one <69286762+Nic98@users.noreply.github.com>
2024-09-24 14:16:22 +0800

    seventh commit

---
+++ .history/build/webpack.base_20240924123003.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.join(__dirname, '../src/index.tsx'), // 入口文件
+  output: {
+    filename: 'static/js/[name].js', // 每个输出js的名称
+    path: path.join(__dirname, '../dist'), // 打包结果输出路径
+    clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
+    publicPath: '/' // 打包后文件的公共前缀路径
+  },
+  resolve: {
+    extensions: ['.js', '.tsx', '.ts'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html'), // 模板取定义root节点的模板
+      inject: true, // 自动注入静态资源
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /.(css|less)$/, //匹配 css和less 文件
+        use: [
+          'style-loader',
+          'css-loader',
+          // 新增
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: ['autoprefixer']
+              }
+            }
+          },
+          'less-loader'
+        ]
+      }
+
+作者：Ausra无忧
+链接：https://juejin.cn/post/7111922283681153038
+来源：稀土掘金
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+      // configure rules below:
+      {
+        test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // 预设执行顺序由右往左,所以先处理ts,再处理jsx
+            presets: [
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          'style-loader',
+          // 将 CSS 转化成 CommonJS 模块
+          'css-loader',
+          // 将 Sass 编译成 CSS
+          'sass-loader',
+        ],
+      },
+    ]
+  }
+};
---
+++ .history/build/webpack.base_20240924124325.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.join(__dirname, '../src/index.tsx'), // 入口文件
+  output: {
+    filename: 'static/js/[name].js', // 每个输出js的名称
+    path: path.join(__dirname, '../dist'), // 打包结果输出路径
+    clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
+    publicPath: '/' // 打包后文件的公共前缀路径
+  },
+  resolve: {
+    extensions: ['.js', '.tsx', '.ts'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html'), // 模板取定义root节点的模板
+      inject: true, // 自动注入静态资源
+    })
+  ],
+  module: {
+    rules: [
+      // configure rules below:
+      {
+        test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // 预设执行顺序由右往左,所以先处理ts,再处理jsx
+            presets: [
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // 将 JS 字符串生成为 style 节点
+          'style-loader',
+          // 将 CSS 转化成 CommonJS 模块
+          'css-loader',
+          // 将 Sass 编译成 CSS
+          'sass-loader',
+        ],
+      },
+    ]
+  }
+};
